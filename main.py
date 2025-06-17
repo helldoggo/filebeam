b730532a1641c83c85a3a6859bbe85bf09e197c3
helldoggo <jhd49er@yahoo.com>
2025-06-17 16:56:10 -0500

    Brainstorming the basic functions of the program, got file listing and passing a directory as an argument functional. Going to do some research on urwid library now to begin UI implementation

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+from os.path import isdir
 import urwid
 import os
 import shutil
 import argparse
 
 def main():
-   #Class for list items
+    #Class for list items
     class Item:
         def __init__(self, path):
             self.path = path
@@ -13,19 +14,52 @@ def main():
             self.type = "Directory" if self.is_dir else "File"
             self.size = os.path.getsize(path) if not self.is_dir else 0
 
+            
+        def check_if_file(self):
+            if self.type == "File":
+                pass
+            else:
+                total_size = 0
+                for root, dirs, files in os.walk(self.path):
+                    for file in files:
+                        try:
+                            full_path = os.path.join(root,file)
+                            total_size += os.path.getsize(full_path)
+                        except:
+                            continue
+                self.size = total_size
+                
+
+                
    #Argument parser for starting point
     starting_parser = argparse.ArgumentParser(description="Starting point for disk analysis")
     starting_parser.add_argument("path", nargs="?", default="/", help="Starting folder")
-
     args = starting_parser.parse_args()
-
     print(f"Starting at: {args.path}")
+
+    cwd = args.path
+   
+    entries = os.listdir(cwd)
+    
+    items = []
+
+    #Object initialization
+    for entry in entries:
+        full_path = os.path.join(cwd, entry)
+        if os.path.exists(full_path):
+            item = Item(full_path)
+            items.append(item)
+
+    for i in items:
+        i.check_if_file()
+#       if i.size > 1024:
+#           i.size = i.size / 1024
+        print(f"{i.name}, {i.type},{i.size / 1024} megabytes")
     
 
-    os.chdir(args.path)
 
-    print(os.path.curdir)
 
+    
 main()
 
     
